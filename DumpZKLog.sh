9008e18f92405960907a8fcda8ef505dea0c43e2
Anthony Rojas <anthony@cloudera.com>
2016-04-25 13:32:39 -0700

    Added first version of DumpZKLog and DumpZKSnapshot

---
+++ DumpZKLog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Dumps latest ZK log file for CM/CDH-based Hadoop Distributions
+# Parcel-based install (Default)
+JARPATH1=/opt/cloudera/parcels/CDH/jars
+ZKPATH1=$JARPATH1
+
+# Comment out the above and uncomment below for package-based installs
+# Package-based install
+#JARPATH1=/usr/lib/zookeeper/lib
+#ZKPATH1=$JARPATH1/..
+
+# Set path to ZK working folder
+ZKLIB1=/var/lib/zookeeper/version-2
+
+# Set path to required CDH jars
+CLPATH1=$(ls -f $ZKPATH1/zookeeper-*cdh*.jar $JARPATH1/log4j-*.jar $JARPATH1/slf4j-log4j*.jar $JARPATH1/slf4j-api-*.jar | tr '\n' ':')
+
+# Set path to Java.  Needs 1.7 or higher
+JHOME=/usr/java/jdk1.7.*/bin
+
+# Detect latest ZK log file
+FILE1=$(ls -tr $ZKLIB1/log* | tail -1)
+
+# Call SnapshotFormatter
+echo "Running LogFormatter..."
+
+$JHOME/java -cp $CLPATH1 org.apache.zookeeper.server.LogFormatter $FILE1
+
+exit
---
+++ DumpZKSnapshot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Dumps latest ZK snapshot for CM/CDH-based Hadoop Distributions
+# Running 'DumpZKSnapshot.sh -all' will dump all contents read from snapshot file
+# Running without the '-all' arg will just report znodes
+
+# Parcel-based install (Default)
+JARPATH1=/opt/cloudera/parcels/CDH/jars
+ZKPATH1=$JARPATH1
+
+# Comment out the above and uncomment below for package-based installs
+# Package-based install
+#JARPATH1=/usr/lib/zookeeper/lib
+#ZKPATH1=$JARPATH1/..
+
+# Set path to ZK working folder
+ZKLIB1=/var/lib/zookeeper/version-2
+
+# Set path to required CDH jars
+CLPATH1=$(ls -f $ZKPATH1/zookeeper-*cdh*.jar $JARPATH1/log4j-*.jar $JARPATH1/slf4j-log4j*.jar $JARPATH1/slf4j-api-*.jar | tr '\n' ':')
+
+# Set path to Java.  Needs 1.7 or higher
+JHOME=/usr/java/jdk1.7.*/bin
+
+# Detect latest snapshot file
+FILE1=$(ls -tr $ZKLIB1/snapshot* | tail -1)
+
+# Assemble runtime command
+EXEC1="$JHOME/java -cp $CLPATH1 org.apache.zookeeper.server.SnapshotFormatter $FILE1 "
+
+# Call SnapshotFormatter
+echo "Running SnapshotFormatter..."
+
+if [ "$1" = "--all" ]; then
+   $EXEC1
+else
+   $EXEC1 | grep '/'
+fi
+
+exit
